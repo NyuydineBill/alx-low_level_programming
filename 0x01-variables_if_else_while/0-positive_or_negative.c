bb555ea32fe35dca92ed60293746a70adbc12b1d
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-10-09 22:08:36 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,9 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/* This program generates a random number and determines if it is positive, negative, or zero. */
+/* This program generates a random number
+and 
+determines if it is positive, negative, or zero. */
 int main(void)
 {
 int n;
