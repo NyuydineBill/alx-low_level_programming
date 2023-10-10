42d653d412c7d56fa73de2afe443b2ee4ee3db5e
NyuydineBill <billleynyuy@gmail.com>
2023-10-09 19:42:20 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int num1, num2;
+for (num1 = 0; num1 < 10; num1++)
+{
+for (num2 = num1 + 1; num2 < 10; num2++)
+{
+putchar(num1 + '0');
+putchar(num2 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
