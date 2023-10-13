027639d70cce5f5dc76efb1e60f82a5a77eab5a6
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-10-13 22:58:48 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
 /**
- * main - Test fuction for positive or negative
+ * main - main
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+int i;
+i = 0;
 
-	i = 0;
-	positive_or_negative(i);
+positive_or_negative(i);
 
 return (0);
+
 }
