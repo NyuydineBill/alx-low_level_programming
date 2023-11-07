bdc592b752a78748ff3ed73472e69fe31a74fe93
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-11-08 00:23:25 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,15 @@
-#include <stddef.h>
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+
 /**
- * struct dog - contains dog information
+ * struct dog - Contains dog information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
  *
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Description: hold all data needed for dog
+ * Description: Holds all data needed for a dog
  */
 struct dog
 {
@@ -17,13 +17,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
+
 #endif /* DOG_H */
