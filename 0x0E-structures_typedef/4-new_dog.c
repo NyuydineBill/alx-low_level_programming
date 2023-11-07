2c2ce9db1ebe380dce25b22bf91910e2d85e9ad3
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-11-08 00:33:12 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * new_dog - new element of type struct dog
- *
+ * new_dog - create a new element of type struct dog
  * @name: name of new dog
  * @age: age of new dog
  * @owner: owner of new dog
- * Return: new struct dog
+ *
+ * Return: pointer to the new struct dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -16,45 +17,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-		return (new_dog);
-	dog_name = malloc(sizeof(name));
-		if (dog_name == NULL)
-		{
-			free(new_dog);
-			return (NULL);
-		}
-	dog_owner = malloc(sizeof(owner));
+		return (NULL);
+
+	dog_name = malloc(strlen(name) + 1);
+	if (dog_name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+
+	dog_owner = malloc(strlen(owner) + 1);
 	if (dog_owner == NULL)
 	{
 		free(dog_name);
 		free(new_dog);
 		return (NULL);
 	}
-	_strcpy(dog_name, name);
-	_strcpy(dog_owner, owner);
+
+	strcpy(dog_name, name);
+	strcpy(dog_owner, owner);
+
 	new_dog->name = dog_name;
 	new_dog->owner = dog_owner;
 	new_dog->age = age;
+
 	return (new_dog);
-}
-
-/**
- * *_strcpy - copies string to given memory location
- * @dest: where the string needs to be copied
- * @src: where the string is
- *
- * Return: char
- */
-char *_strcpy(char *dest, char *src)
-{
-	int len = 0;
-
-	while (*(src + len) != '\0')
-	{
-		*(dest + len) = *(src + len);
-		len++;
-	}
-	*(dest + len) = *(src + len);
-
-	return (dest);
 }
