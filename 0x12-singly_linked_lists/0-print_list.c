fd59a668e895450c9c26742c772d326cf9f16cdf
Nyuydine Bill leynyuy <121938133+NyuydineBill@users.noreply.github.com>
2023-11-17 19:57:14 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,14 @@
 #include "lists.h"
-/**
- * Done by Bill
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
 
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ * Return: number of elements in the list
+ */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t nelem = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
